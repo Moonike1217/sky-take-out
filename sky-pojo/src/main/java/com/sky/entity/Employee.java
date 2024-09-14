55042bfd20a038c5724acbfcdb3e3547af806f74
Moonike <2462599013@qq.com>
2024-09-14 15:17:27 +0800

    业务开发-员工分页查询

--- sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.sky.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -32,10 +33,10 @@ public class Employee implements Serializable {
 
     private Integer status;
 
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") //通过注解的形式来格式化时间
     private LocalDateTime createTime;
 
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") //通过注解的形式来格式化时间
     private LocalDateTime updateTime;
 
     private Long createUser;
